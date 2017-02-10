5dca3fb999a4f09790bb538762dc85330b5cd50a
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-02-09 20:25:09 -0800

    Added second method

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var pikesPlace = {
   min: 17,
   max: 88,
-  avgCookieSale: 5.2,
-  timeOfDay: ['Ten:', 'Eleven:', 'Twelve:', 'One:', 'Two:', 'Three:', 'Four:', 'Five:']
-  randCustPerHour: function (min, max) {
-    return Math.random() * (max - min) + min
+  avg: 5.2,
+  timeOfDay: ['Ten:', 'Eleven:', 'Twelve:', 'One:', 'Two:', 'Three:', 'Four:', 'Five:'],
+
+  randCustPerHour: function(min, max) {
+    return Math.random() * (max - min + 1) + min;
   },
-  // other methods to aggregate data...
-  numberCookiesPerHour: function ()
-  render: function() {
-    // render your object data to the DOM as an unordered list of data points!!
-    // 1. Get an element by Id from your html
-    // 2. Create a UL element
-    // 3. Create and populate an LI element for each of the data points that you want to render
-    // 4. Append the LI to the UL
-    // 5. Append the UL to the element that you stored in step 1
-  }
+
+  numberCookiesPerHour: function() {
+    for (i = 0; i < 7; i++) {
+      document.write(pikesPlace.timeOfDay[i] + Math.floor(this.randCustPerHour(pikePlace.min, pikePlace.max)*(pikePlace.avg)))
+    }
+
+  },
+  liEl: document.getElementById("timeOfDay"),
+  timeOfDay.textContent = pikePlace.render(),
 }
