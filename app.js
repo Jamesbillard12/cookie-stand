8eb8470128b6a9f018d8adbfe3f38bee4e725a8d
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-02-09 22:20:19 -0800

    finished most first store

--- app.js
+++ app.js
@@ -8,18 +8,27 @@ var pikesPlace = {
   randCustPerHour: function() {
     return Math.random() * (this.max - this.min + 1) + this.min;
   },
-
+  total: 0,
   cookiesPurchased: function() {
     for ( var i = 0; i < this.timeOfDay.length; i++) {
-      this.cookiesPurchasedArray.push(this.avg * this.randCustPerHour());
+      var rand = this.avg * this.randCustPerHour();
+      this.cookiesPurchasedArray.push(rand);
+      this.total += rand;
     }
   },
-  numberCookiesPerHour: function() {
-    for (var i = 0; i < 7; i++) {
+  renderCookiesPerHour: function() {
+    this.cookiesPurchased();
+    var ul = document.getElementById('pikePlace');
+    for (var i = 0; i < this.timeOfDay.length; i++) {
       var li = document.createElement("li");
-      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.randCustPerHour(this.min, this.max)*(this.avg))));
-      ul.appendChild(li)
-    }
+      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.cookiesPurchasedArray[i])));
+      ul.appendChild(li);
 
+    }
+    var li2 = document.createElement("li");
+    li2.appendChild(document.createTextNode("Total: " + Math.floor(this.total)));
+    ul.appendChild(li2);
   },
 }
+
+// this.timeOfDay[i] + Math.floor
