5f972c4974146acf049de908180d945e1e3e8d38
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-02-11 00:01:31 -0800

    finished Bellevue object

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ var southCenter = {
   },
   cookiesPurchased: function() {
     for (var i = 0; i < this.timeOfDay.length; i++) {
-      var rand = this.avg * this. randCustPerHour();
+      var rand = this.avg * this.randCustPerHour();
       this.cookiesPurchasedArray.push(rand);
       this.total += rand;
     }
@@ -104,5 +104,41 @@ var southCenter = {
     var li2 = document.createElement('li');
     li2.appendChild(document.createTextNode('Total: ' + Math.floor(this.total)));
     ul.appendChild(li2);
-  }
-}
+  },
+};
+
+var bellevueSquare = {
+  min: 20,
+  max: 48,
+  avg: 3.3,
+  total: 0,
+  timeOfDay: ['Ten: ', 'Eleven: ', 'Twelve, ', 'One: ', 'Two, ', 'Three, ', 'Four: ', 'Five: '],
+  cookiesPurchasedArray: [],
+
+  randCustPerHour: function() {
+    return Math.random() * (this.max - this.min + 1) + this.min;
+  },
+  cookiesPurchased: function(){
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var rand = this.avg * this.randCustPerHour();
+      this.cookiesPurchasedArray.push(rand);
+      this.total += rand;
+    }
+  },
+
+  renderCookiesPerHour: function() {
+    this.cookiesPurchased();
+    var ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    ul.appendChild(document.createTextNode('Bellevue Square'));
+
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.cookiesPurchasedArray[i])));
+      ul.appendChild(li);
+    }
+    var li2 = document.createElement('li');
+    li2.appendChild(document.createTextNode('Total: ' + Math.floor(this.total)));
+    ul.appendChild(li2);
+  },
+};
