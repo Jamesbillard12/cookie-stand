2283b7d2f699f54374eb2ff082a5da7483b63c13
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-02-11 00:22:51 -0800

    finished alki object

--- app.js
+++ app.js
@@ -142,3 +142,39 @@ var bellevueSquare = {
     ul.appendChild(li2);
   },
 };
+
+var alki = {
+  min: 3,
+  max: 24,
+  avg: 2.6,
+  total: 0,
+  timeOfDay: ['Ten; ', 'Eleven: ', 'Twelve: ', 'One: ', 'Two: ', 'Three: ', 'Four: ', 'Five: '],
+  cookiesPurchasedArray: [],
+
+  randCustPerHour: function() {
+    return Math.random() * (this.max - this.min + 1) + this.min;
+  },
+  cookiesPurchased: function(){
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var rand = this.avg * this.randCustPerHour();
+      this.cookiesPurchasedArray.push(rand);
+      this.total += rand;
+    }
+  },
+
+  renderCookiesPerHour: function() {
+    this.cookiesPurchased();
+    var ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    ul.appendChild(document.createTextNode('Alki'));
+
+    for (var i = 0; i < this.timeOfDay.length; i++) {
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.cookiesPurchasedArray[i])));
+      ul.appendChild(li);
+    }
+    var li2 = document.createElement('li');
+    li2.appendChild(document.createTextNode('Total: ' + Math.floor(this.total)));
+    ul.appendChild(li2);
+  },
+};
