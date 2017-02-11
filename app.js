86f1aff388f1e8440577b12b57e600e837c106ab
James R Billard <jamesrbillard@Jamess-MacBook-Pro.local>
2017-02-10 22:24:19 -0800

    created parent ul in JS all works

--- app.js
+++ app.js
@@ -18,20 +18,18 @@ var pikePlace = {
   },
   renderCookiesPerHour: function() {
     this.cookiesPurchased();
-    var idPikePlace = document.createElement('ul');
-    idPikePlace.id ='pikePlace';
-    var body = document.querySelector('body');
-    idPikePlace = document.createTextNode('Pike Place');
-    body.appendChild(idPikePlace);
-    var ul = document.getElementById('pikePlace');
+    var ul = document.createElement('ul');
+    document.body.appendChild(ul);
+    ul.appendChild(document.createTextNode('Pike Place'));
+
     for (var i = 0; i < this.timeOfDay.length; i++) {
       var li = document.createElement("li");
       li.appendChild(document.createTextNode(this.timeOfDay[i] + Math.floor(this.cookiesPurchasedArray[i])));
-      idPikePlace.appendChild(li);
+      ul.appendChild(li);
 
     }
     var li2 = document.createElement("li");
     li2.appendChild(document.createTextNode("Total: " + Math.floor(this.total)));
-    idPikePlace.appendChild(li2);
+    ul.appendChild(li2);
   },
 }
